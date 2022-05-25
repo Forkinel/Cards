6e8ae58a51027fe998202747ac4d73567bc2e33e
2030lab <bob.score@2030lab.com>
2022-05-25 14:23:05 +0100

    intital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  posts = [
+    {
+      title: 'neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw a neat tree today'
+    },
+    {
+      title: 'snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today'
+    }
+  ];
+}
